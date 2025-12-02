b8f02db250d2969ecd959b6521334f7be6442129
Lucas <lucas@example.com>
2025-12-02 10:47:06 -0300

    Fix(api): configurar DRF para usar AutoSchema de drf-spectacular

--- modulos_consultas/settings.py
+++ modulos_consultas/settings.py
@@ -299,6 +299,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Django REST Framework settings
 REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
         'rest_framework.authentication.SessionAuthentication',
